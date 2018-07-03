db5dc954ec7e88a81166aa13a70a15920c3c91cd
Mark Bellingham <mark.bellingham@protonmail.ch>
2018-07-03 15:37:24 +0100

    added custom 404 page

---
+++ portfolio/404.php
@@ -0,0 +1,16 @@
+<?php
+$title = "Computing Graduate";
+include_once("portfolio/includes/header.php");
+?>
+<article style="min-height: 100%;">
+  <center>
+    <div>
+      <h2>404 - Sorry something went wrong.</h2>
+        Like a badly spelled variable this page does not exist.<br/>
+        Please use the links at the top to find what you're looking for.
+    </div>
+  </center>
+</article>
+<?php
+include_once("portfolio/includes/footer.php");
+?>
