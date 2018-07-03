3c9ec2ea02515bcad889455ea96fe9cdab04f140
Mark Bellingham <mark.bellingham@protonmail.ch>
2018-07-03 15:33:53 +0100

    moved style changer radion buttons to here

--- portfolio/includes/footer.php
+++ portfolio/includes/footer.php
@@ -2,10 +2,10 @@
       <section>
         <div style="clear:both; display: inline;">
             <b>Change the website style </b>
-            <input type="radio" name="grootte" value="1" onclick="SetCookie(nameOfCookie, this.value, exp); doRefresh();"> Style 1 &nbsp;</input>
-            <input type="radio" name="grootte" value="2" onclick="SetCookie(nameOfCookie, this.value, exp); doRefresh();"> Style 2 &nbsp;</input>
-            <input type="radio" name="grootte" value="3" onclick="SetCookie(nameOfCookie, this.value, exp); doRefresh();"> Style 3 &nbsp;</input>
-            <input type="radio" name="grootte" value="4" onclick="SetCookie(nameOfCookie, this.value, exp); doRefresh();"> Style 4 &nbsp;</input>
+            <input type="radio" name="grootte" value="1" onclick="SetCookie(nameOfCookie, this.value, exp); doRefresh();"/> Style 1 &nbsp;
+            <input type="radio" name="grootte" value="2" onclick="SetCookie(nameOfCookie, this.value, exp); doRefresh();"/> Style 2 &nbsp;
+            <input type="radio" name="grootte" value="3" onclick="SetCookie(nameOfCookie, this.value, exp); doRefresh();"/> Style 3 &nbsp;
+            <input type="radio" name="grootte" value="4" onclick="SetCookie(nameOfCookie, this.value, exp); doRefresh();"/> Style 4 &nbsp;
         </div>
       </section>
     </footer>
