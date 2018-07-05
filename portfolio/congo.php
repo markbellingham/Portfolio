f493c0d021eb392081bc02dc86ed2dafff75fd0e
Mark Bellingham <mark.bellingham@protonmail.ch>
2018-07-05 11:59:04 +0100

    updated congo screenshots

--- portfolio/congo.php
+++ portfolio/congo.php
@@ -8,59 +8,68 @@ ga('create', 'UA-115626803-1', 'auto');
 
 <h1>Congo</h1>
 <div class="left">
-<p>
-  2nd year Web and Mobile assignment comprising a dynamic website linking html/css, java, and mysql.
-</p>
-<p>
-  The brief was to create a dynamic website which would take data from a database and display relevant content in the context of an online shop. The technologies that are used in this project are MySQL for the database and a combination of Java servlets and JSP for the front end.
-</p>
-<p>
-  <a href="documents/web-mobile-assignment-report.pdf" target="_blank"
-    onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'PDF document', 'eventAction':'assignment-report-congo'});">
-    <b>Assignment report</b>
-  </a>
-</p>
-<p>
-  <a class="applink" href="https://markbellingham.me/Congo" target="_blank"
-      onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'application demo', 'eventAction':'app-Congo'});">
-    Try out the website here
-  </a>
-</p>
-<p>
-  Some of the features of this web application will require you to log in before they are accessible<br/>
-  <a href="https://markbellingham.me/portfolio/documents/congo_passwords.txt" target="_blank"><b>You can find a list of current users and their passwords here</b></a>
-</p>
-<p>
-  If you want to create this project for yourself
-  <a href="https://markbellingham.me/wiki/index.php?title=Create_an_object_oriented_dynamic_website_with_Java_and_MySQL">
-    <b>try out my tutorial!</b>
-  </a>
-</p>
-<p>
-  <a href="https://github.com/markbellingham/congo" target="_blank"
-      onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'code', 'eventAction':'GitHub-Congo'});">
-    <b>Link to the project source code</b>
-  </a>
-</p>
-<h2>Features include</h2>
-<h3>Object Oriented MVC Framework</h3>
-<h3>Search options by category, price, artist, or by text</h3>
-<h3>Login and register</h3>
-<h3>Shopping basket and save orders to the database</h3>
-<h3>Retrieve previous orders from the database</h3>
-<h3>Admin features - delete, insert new, and modify</h3>
+  <p>
+    A rewrite of the 2nd year Web and Mobile assignment comprising a dynamic website linking JSP/JSTL, Java, and MySQL.
+  </p>
+  <p>
+    The brief was to create a dynamic website which would take data from a database and display relevant content in the context of an online shop. The technologies that are used in this project are MySQL for the database and a combination of Java objects, Java servlets, JSP, and JSTL for the front end.
+      <a href="https://markbellingham.me/wiki/index.php?title=Create_an_object_oriented_dynamic_website_with_Java_and_MySQL" target="_blank"
+        onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'wiki', 'eventAction':'Tutorial-Congo'});">
+        <b>There is also a tutorial for this project in my wiki</b>
+      </a>.
+  </p>
+  <p>
+    <a href="documents/web-mobile-assignment-report.pdf" target="_blank"
+      onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'PDF document', 'eventAction':'assignment-report-congo'});">
+      <b>Assignment report</b>
+    </a>
+  </p>
+  <p>
+    <a class="applink" href="https://markbellingham.me/Congo" target="_blank"
+        onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'application demo', 'eventAction':'app-Congo'});">
+      Try out the website here
+    </a>
+  </p>
+  <p>
+    Some of the features of this web application will require you to log in before they are accessible<br/>
+    <a href="https://markbellingham.me/portfolio/documents/congo_passwords.txt" target="_blank"><b>You can find a list of current users and their passwords here</b></a>
+  </p>
+  <p>
+    If you want to create this project for yourself
+    <a href="https://markbellingham.me/wiki/index.php?title=Create_an_object_oriented_dynamic_website_with_Java_and_MySQL" target="_blank"
+        onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'wiki', 'eventAction':'Tutorial-Congo'});">
+      <b>try out my tutorial!</b>
+    </a>
+  </p>
+  <p>
+    <a href="https://github.com/markbellingham/Congo-Tutorial" target="_blank"
+        onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'code', 'eventAction':'GitHub-Congo'});">
+      <b>Link to the project source code</b>
+    </a>
+  </p>
+  <h2>Features include:</h2>
+  <p>Object Oriented MVC Framework</p>
+  <p>Search options by category, price, artist, or by text</p>
+  <p>Dynamically created links for artist, album, and category</p>
+  <p>Login, logout, and register</p>
+  <p>Shopping basket including update quantities and delete</p>
+  <p>Save orders to the database</p>
+  <p>Retrieve previous orders from the database</p>
+  <p>Admin features - delete, insert new, and modify</p>
 </div> <!-- ends left -->
 <div class="right">
-  <a data-fancybox="gallery" class="congo-img" href="images/screens/Category-Classical.png" onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'images', 'eventAction':'app-Congo'});">
-    <img src="images/screens/Category-Classical-sm.png" width="100%" alt="Categories - Classical"/>
+  <a data-fancybox="gallery" class="congo-img" href="images/screens/congo-search-by-category.png" onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'images', 'eventAction':'app-Congo'});">
+    <img src="images/screens/congo-search-by-category-sm.png" width="100%" alt="Categories - Jazz"/>
   </a>
-  <a data-fancybox="gallery" class="congo-img" href="images/screens/Search-By-Keyword.png" onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'images', 'eventAction':'app-Congo'});">
-    <img src="images/screens/Search-By-Keyword-sm.png" width="100%" alt="Search By Keyword"/>
+  <a data-fancybox="gallery" class="congo-img" href="images/screens/congo-search-by-keyword.png" onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'images', 'eventAction':'app-Congo'});">
+    <img src="images/screens/congo-search-by-keyword-sm.png" width="100%" alt="Search By Keyword"/>
   </a>
-  <a data-fancybox="gallery" class="congo-img" href="images/screens/Shopping-Basket.png" onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'images', 'eventAction':'app-Congo'});">
-    <img src="images/screens/Shopping-Basket-sm.png" width="100%" alt="Shopping Basket"/>
+  <a data-fancybox="gallery" class="congo-img" href="images/screens/congo-shopping-basket.png" onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'images', 'eventAction':'app-Congo'});">
+    <img src="images/screens/congo-shopping-basket-sm.png" width="100%" alt="Shopping Basket"/>
   </a>
-  <a data-fancybox="gallery" class="congo-img" href="images/screens/Login.png" onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'images', 'eventAction':'app-Congo'});">
-    <img src="images/screens/Login-sm.png" width="100%" alt="Login Screen"/>
+  <a data-fancybox="gallery" class="congo-img" href="images/screens/congo-login.png" onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'images', 'eventAction':'app-Congo'});">
+    <img src="images/screens/congo-login-sm.png" width="100%" alt="Login Screen"/>
+    <a data-fancybox="gallery" class="congo-img" href="images/screens/congo-admin-controls.png" onclick="ga(‘send’, {'hitType':'event', 'eventCategory':'images', 'eventAction':'app-Congo'});">
+      <img src="images/screens/congo-admin-controls-sm.png" width="100%" alt="Admin Controls"/>
   </a>
 </div> <!-- ends right -->
