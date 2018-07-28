d13a77e2ca21762e284c474b61604cfd4192924b
Mark Bellingham <mark.bellingham@protonmail.ch>
2018-07-28 11:18:54 +0100

    added 404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+$title = "Computing Graduate";
+include_once("portfolio/includes/header.php");
+?>
+<body>
+  <center>
+    <div style="height: 100%;">
+      <h2>404 - Sorry something went wrong.</h2>
+        Like a badly spelled variable this page does not exist.<br/>
+        Please use the links at the top to find what you're looking for.
+    </div>
+  </center>
+</body>
+<?php
+include_once("portfolio/includes/footer.php");
+?>
